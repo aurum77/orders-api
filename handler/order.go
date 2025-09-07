633bc8c94df2a046566922e00296a44fd7354542
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-09-07 17:21:26 +0300

    create handlers for orders

--- handler/order.go
+++ handler/order.go
@@ -6,3 +6,23 @@ import (
 )
 
 type Order struct{}
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("create an order")
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("list orders")
+}
+
+func (o *Order) GetById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get order by id")
+}
+
+func (o *Order) UpdateById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("update order by id")
+}
+
+func (o *Order) DeleteById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("delete order by id")
+}
